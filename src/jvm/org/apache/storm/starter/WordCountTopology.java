ce2d92ba929c50490c2e858fc0e776895a63f872
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.trl.ibm.com>
2017-02-08 14:16:42 +0900

    updated

--- src/jvm/org/apache/storm/starter/WordCountTopology.java
+++ src/jvm/org/apache/storm/starter/WordCountTopology.java
@@ -17,23 +17,21 @@
  */
 package org.apache.storm.starter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.StormSubmitter;
-import org.apache.storm.task.ShellBolt;
+import org.apache.storm.starter.spout.RandomSentenceSpout;
+import org.apache.storm.starter.spout.ZipfGeneratorSpout;
 import org.apache.storm.topology.BasicOutputCollector;
-import org.apache.storm.topology.IRichBolt;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
-import org.apache.storm.starter.spout.RandomSentenceSpout;
-import org.apache.storm.starter.spout.ZipfGeneratorSpout;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * This topology demonstrates Storm's stream groupings and multilang capabilities.
