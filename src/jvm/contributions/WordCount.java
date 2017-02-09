f85c6bf497df39e07f443be362522ff277d35183
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-02-09 17:16:52 +0900

    Addede code

--- src/jvm/contributions/WordCount.java
+++ src/jvm/contributions/WordCount.java
@@ -3,29 +3,49 @@ package contributions;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.IRichBolt;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseBasicBolt;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
-public class WordCount extends BaseBasicBolt {
+public class WordCount implements IRichBolt {
     Map<String, Integer> counts = new HashMap<String, Integer>();
-
-    @Override
-    public void execute(Tuple tuple, BasicOutputCollector collector) {
-      String word = tuple.getString(0);
-      Integer count = counts.get(word);
-      if (count == null)
-        count = 0;
-      count++;
-      counts.put(word, count);
-      //collector.emit(new Values(word, count));
-    }
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
       declarer.declare(new Fields("word", "count"));
     }
+
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void execute(Tuple tuple) {
+		String word = tuple.getString(0);
+	      Integer count = counts.get(word);
+	      if (count == null)
+	        count = 0;
+	      count++;
+	      counts.put(word, count);
+	}
+
+	@Override
+	public void prepare(Map arg0, TopologyContext arg1, OutputCollector arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
   }
