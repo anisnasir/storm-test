7bbdd0de0f210539e97946dfddc07bd182e53de1
Muhammad Anis Uddin Nasir <Anis@Muhammads-MacBook-Pro.local>
2017-02-17 11:06:19 +0900

    added code

--- src/jvm/contributions/RelaxedConsistentGrouping.java
+++ src/jvm/contributions/RelaxedConsistentGrouping.java
@@ -95,7 +95,7 @@ public class RelaxedConsistentGrouping implements LoadAwareCustomStreamGrouping,
                 raw = values.get(0).toString().getBytes(); // assume key is the first field
             }
             
-        /*    if ((lastUpdate + 60000) < System.currentTimeMillis()) {
+            if ((lastUpdate + 60000) < System.currentTimeMillis()) {
             	//add increase load and decrease load logic
             	for (int i = 0; i < targetTasks.size(); i++) {
                     double val = load.get(targetTasks.get(i));
@@ -107,7 +107,7 @@ public class RelaxedConsistentGrouping implements LoadAwareCustomStreamGrouping,
                     
             	}
                 lastUpdate = System.currentTimeMillis();
-            }*/
+            }
             
             int selected = hash.getServer(raw);
             boltIds.add(targetTasks.get(selected));
