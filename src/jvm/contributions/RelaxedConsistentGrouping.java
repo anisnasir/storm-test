1da0fbc3b034fc74aead7dd8ef662c57c6e4238a
Muhammad Anis Uddin Nasir <Anis@Muhammads-MacBook-Pro.local>
2017-02-17 09:52:47 +0900

    added code

--- src/jvm/contributions/RelaxedConsistentGrouping.java
+++ src/jvm/contributions/RelaxedConsistentGrouping.java
@@ -49,7 +49,7 @@ public class RelaxedConsistentGrouping implements LoadAwareCustomStreamGrouping,
     @Override
     public void prepare(WorkerTopologyContext context, GlobalStreamId stream, List<Integer> targetTasks) {
         this.targetTasks = targetTasks;
-        hash = new RelaxedConsistentHashing(targetTasks.size(), 1000);
+        hash = new RelaxedConsistentHashing(targetTasks.size(), 10);
         if (this.fields != null) {
             this.outFields = context.getComponentOutputFields(stream);
         }
