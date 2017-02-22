ff16dc4b2f5c3753702890a1af11b782a2de45fd
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-02-22 16:32:45 +0900

    added code

--- src/jvm/contributions/RelaxedConsistentGrouping.java
+++ src/jvm/contributions/RelaxedConsistentGrouping.java
@@ -100,7 +100,7 @@ public class RelaxedConsistentGrouping implements LoadAwareCustomStreamGrouping,
                 raw = values.get(0).toString().getBytes(); // assume key is the first field
             }
             
-            if ((lastUpdate + 30000) < System.currentTimeMillis()) {
+            if ((lastUpdate + 10000) < System.currentTimeMillis()) {
             	//add increase load and decrease load logic
             	for (int i = 0; i < targetTasks.size(); i++) {
                     double val = load.get(targetTasks.get(i));
