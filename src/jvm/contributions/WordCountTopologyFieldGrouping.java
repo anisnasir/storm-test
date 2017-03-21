74074927827dde76f4aa34c735476a6949a429e1
Muhammad Anis Uddin Nasir <Anis@muhammads-mbp.dhcp.hakozaki.ibm.com>
2017-03-21 10:04:37 +0900

    added code

--- src/jvm/contributions/WordCountTopologyFieldGrouping.java
+++ src/jvm/contributions/WordCountTopologyFieldGrouping.java
@@ -55,6 +55,7 @@ public class WordCountTopologyFieldGrouping {
     String topicName= "test";
     BrokerHosts hosts = new ZkHosts(zkConnString);
     SpoutConfig spoutConfig = new SpoutConfig(hosts, topicName, "/" + topicName, UUID.randomUUID().toString());
+    spoutConfig.startOffsetTime = kafka.api.OffsetRequest.EarliestTime();
     spoutConfig.scheme = new SchemeAsMultiScheme(new StringScheme() {
     	@Override
         public Fields getOutputFields() {
